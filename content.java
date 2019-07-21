16aeaf80372a220938ce88838869df61c2fe7811
Philippe <phiberberian@gmail.com>
2019-07-21 02:02:19 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cokqoa {
     public static void main() {
         System.out.println("rtidn");
         System.out.println("bmcmf");
-        System.out.println("vladi");
+        System.out.println("pwmbe");
     }
 }
