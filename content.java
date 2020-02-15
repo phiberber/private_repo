a9a8449c0aff9daf6a7aad390b1e4cc8681db855
Philippe <phiberberian@gmail.com>
2020-02-15 19:49:19 +0000

    add code in files types: .json,.java,.kt remove code in files types: .json,.java,.kt

--- content.java
+++ content.java
@@ -2,6 +2,6 @@ public class Cbffcb {
     public static void main() {
         System.out.println("nqucw");
         System.out.println("waqcl");
-        System.out.println("avstm");
+        System.out.println("melhq");
     }
 }
