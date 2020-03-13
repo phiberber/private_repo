4cba5acef9d8a2bd8569813a3a3ffa24536723e2
Philippe <phiberberian@gmail.com>
2020-03-13 23:25:17 +0000

    add code in files types: remove code in files types: .java

--- content.java
+++ content.java
@@ -3,6 +3,5 @@ public class Cbffcb {
         System.out.println("nqucw");
         System.out.println("waqcl");
         System.out.println("ejcvk");
-        System.out.println("bjjmc");
     }
 }
