ff867bddcc0dfb94a314c67ac0f42204c143208a
Philippe <phiberberian@gmail.com>
2020-06-24 02:54:48 +0000

    add code in files types: .gradle,.java,.kt remove code in files types: .java,.kt

--- content.java
+++ content.java
@@ -9,6 +9,6 @@ public class Cbffcb {
         System.out.println("xhxgs");
         System.out.println("teflj");
         System.out.println("yjkel");
-        System.out.println("kwhco");
+        System.out.println("ecaal");
     }
 }
